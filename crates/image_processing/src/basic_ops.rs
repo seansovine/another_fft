b11f8b098d2e08dfd716fc9a437f7e8d8c23ea31
Sean Sovine <sean.r.sovine@gmail.com>
2025-06-14 08:05:45 -0400

    Add RGBA grayscale conversion image processing feature

---
+++ crates/image_processing/src/basic_ops.rs
@@ -0,0 +1,80 @@
+// Basic image operations like resize, grayscale.
+
+use crate::Image;
+
+// Use the image crate to resize an image.
+//
+// Initially we'll use this to resize an image to power of two dimensions
+// so we can apply the 2-dimensional FFT to it.
+
+pub fn resize(in_path: &str) {
+  let image = Image::from_file(in_path);
+  println!("Image loaded successfully!");
+  println!("Dimensions: {:?}", image.dimensions);
+
+  let new_dimensions: (u32, u32) = (8192, 4096);
+
+  println!("Resizing image...");
+
+  let resize_filter = image::imageops::FilterType::CatmullRom;
+  let buffer = image::imageops::resize(
+    &image.image,
+    new_dimensions.0,
+    new_dimensions.1,
+    resize_filter,
+  );
+
+  println!("Resize success!");
+
+  // save resized image, to hard-coded file for now
+
+  const OUTPATH: &str = "test/resized.jpg";
+  buffer
+    .save_with_format(OUTPATH, image::ImageFormat::Jpeg)
+    .unwrap();
+
+  // view result: wgpu_grapher image --path test/resized.jpg
+}
+
+// Convert image pixels to grayscale but still RGBA representation.
+
+pub fn to_grayscale(in_path: &str) {
+  let mut image = Image::from_file(in_path);
+  let dims = image.dimensions;
+
+  println!("Image loaded successfully!");
+  println!("Dimensions: {:?}", image.dimensions);
+
+  let to_grayscale = |p: &[u8]| -> u8 {
+    ((p[0] as f64 * 0.299) + (p[1] as f64 * 0.587) + (p[2] as f64 * 0.114)) as u8
+  };
+
+  println!("Converting image to grayscale (RGBA)...");
+
+  for i in 0..dims.0 {
+    for j in 0..dims.1 {
+      // account for 4-byte representation and row-column
+      // let pix_offset = (i * 4 + (4 * j * dims.0)) as usize;
+
+      let pixel = &mut image.image[(i, j)].0;
+
+      let grayscale_val = to_grayscale(pixel);
+
+      pixel[0] = grayscale_val;
+      pixel[1] = grayscale_val;
+      pixel[2] = grayscale_val;
+    }
+  }
+
+  println!("Conversion success!");
+
+  // save grayscale image, to hard-coded file for now
+
+  const OUTPATH: &str = "test/grayscale.jpg";
+  image
+    .image
+    .save_with_format(OUTPATH, image::ImageFormat::Jpeg)
+    .unwrap();
+
+  // view result: wgpu_grapher image --path test/grayscale.jpg
+}
